6ab91d4496f42e9eabd55a380f0670b34629d3b2
abi-feetham <abi.feetham@gmail.com>
2020-06-30 22:48:38 +0100

    changed css for pokemon info page

--- pages/components/PokemonInfoComponent.js
+++ pages/components/PokemonInfoComponent.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import styles from "./PokemonInfoComponent.module.css"
 
 const PokemonInfoComponent = (props) => {
     return (
         <div>
+            <div className={styles.pokemoninfobox}>
             <h2>{props.pokemonInfo.name}</h2>
-            <img src={props.pokemonInfo.thumbnail} />
-            <p>ID: {props.pokemonInfo.id}</p>
-            <p>Active: {props.pokemonInfo.active}</p>
-            <p>Level: {props.pokemonInfo.level}</p>
+            <img src={props.pokemonInfo.thumbnail} id={styles.pokemonimage}/>
+            <p id={styles.pokemoninfo}>ID: {props.pokemonInfo.id}<br/>
+            Active: {props.pokemonInfo.active}<br/>
+            Level: {props.pokemonInfo.level}</p>
+            </div>
             <br/>
-            <p>HP: {props.pokemonInfo.hp}</p>
-            <p>Attack: {props.pokemonInfo.atk}</p>
-            <p>Defense: {props.pokemonInfo.def}</p>
-            <p>Sp. Atk: {props.pokemonInfo.spatk}</p>
-            <p>Sp. Def: {props.pokemonInfo.spdef}</p>
-            <p>Speed: {props.pokemonInfo.speed}</p>
+            <div className={styles.statsbox}>
+            <h2 class={styles.stativtitle}>Stats</h2>
+            <p class={styles.stativtextleft}>HP: {props.pokemonInfo.hp}<br/>
+            Attack: {props.pokemonInfo.atk}<br/>
+            Defense: {props.pokemonInfo.def}</p>
+            <p class={styles.stativtextright}>Sp. Atk: {props.pokemonInfo.spatk}<br/>
+            Sp. Def: {props.pokemonInfo.spdef}<br/>
+            Speed: {props.pokemonInfo.speed}</p>
+            </div>
             <br/>
-            <p>HP IV: {props.pokemonInfo.hpIV}</p>
-            <p>Attack IV: {props.pokemonInfo.atkIV}</p>
-            <p>Defense IV: {props.pokemonInfo.defIV}</p>
-            <p>Sp. Atk IV: {props.pokemonInfo.spatkIV}</p>
-            <p>Sp. Def IV: {props.pokemonInfo.spdefIV}</p>
-            <p>Speed IV: {props.pokemonInfo.speedIV}</p>
-            <p>Total IV: {props.pokemonInfo.totalIV}%</p>
-            <style jsx>{`
-                p {
-                    margin: 0;
-                }
-            `}</style>
+            <div className={styles.ivsbox}>
+            <h2 class={styles.stativtitle}>IVs</h2>
+            <p class={styles.stativtextleft}>HP IV: {props.pokemonInfo.hpIV}<br/>
+            Attack IV: {props.pokemonInfo.atkIV}<br/>
+            Defense IV: {props.pokemonInfo.defIV}</p>
+            <p class={styles.stativtextright}>Sp. Atk IV: {props.pokemonInfo.spatkIV}<br/>
+            Sp. Def IV: {props.pokemonInfo.spdefIV}<br/>
+            Speed IV: {props.pokemonInfo.speedIV}</p>
+            <h2 id={styles.totaliv}>Total IV: {props.pokemonInfo.totalIV}%</h2>
+            </div>
         </div>
     )
 }
